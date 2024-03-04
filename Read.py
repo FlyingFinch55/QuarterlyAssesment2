15dba95ca7ecc8bed7ea22f1d88dcbb7fedfe359
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-04 09:45:31 -0600

    Took my read file from last project and pasted here as a ruff draft for this read file

--- Read.py
+++ Read.py
@@ -1,3 +1,22 @@
 
 #Code to change color back to white
 #print('\033[38;2;225;225;225m')
+
+
+import sqlite3
+
+conn= sqlite3.connect('QuarterA2.db')
+curse = conn.cursor()
+
+curse.execute("SELECT * FROM sqlite_master WHERE type='table'")
+#Reads all table
+print(curse.fetchall())
+print()
+
+curse.execute("SELECT * FROM ACCT")
+print(curse.fetchall())
+print()
+
+curse.execute("SELECT question FROM ACCT WHERE id = 1 ")
+print(curse.fetchall())
+print()
