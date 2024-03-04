57f9d9821137cb1099d00ce0e0c44a240dd4be32
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-04 09:40:48 -0600

    Adding comments for my readablilty

--- Create.py
+++ Create.py
@@ -5,20 +5,31 @@ import random
 conn= sqlite3.connect('QuarterA2.db')
 curse = conn.cursor()
 
+#Table 1 labled ACCT
 curse.execute('''CREATE TABLE IF NOT EXISTS ACCT
               question TEXT, answer TEXT''')
 
+
+#Table 2 labled Databaase
 curse.execute('''CREATE TABLE IF NOT EXISTS Database
               question TEXT, answer TEXT''')
 
+
+
+#Table 3 labeld AssablyProg
 curse.execute('''CREATE TABLE IF NOT EXISTS AssablyProg
               question TEXT, answer TEXT''')
 
+
+
+#Table 4 labeled PythonProg
 curse.execute('''CREATE TABLE IF NOT EXISTS PythonProg
               question TEXT, answer TEXT''')
 
+
+#Table 5 labled ComputerHardWear
 curse.execute('''CREATE TABLE IF NOT EXISTS ComputerHardWear
               question TEXT, answer TEXT''')
 
 # make a question array to store questions and answers as normal strings
-questions = []
+#questions = []
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # QuarterlyAssesment2
+
+
+There are three files, the Create Add and Read python files.
+the only one the user needs to access is the Add file. 
+The add file is used to run the quiz bowl
